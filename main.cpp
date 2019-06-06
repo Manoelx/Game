efbb5ca2c1c80cda9acbac78f90abeb707e7a670
Manoelx <49821697+Manoelx@users.noreply.github.com>
2019-06-06 19:13:53 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,53 +1,70 @@
-#include "aux.h"
+#include <iostream>
+using namespace std;
 
-      //config_jogo(int q_cores, int codigo, int palpites,int select, bool repetidas)
+
+class Bola{
+private:
+	string cor;
+public:
+	void set_cor(string color){cor=color;}
+	string get_cor(void){return cor;}
+};
+class jogo_pc{
+	private:
+		Bola b[9][4];
+		string cores[];
+	public:
+		void set_posi(int linha, int p, string color){
+			while(1){				
+			if(cor_pertence(color){
+				b[linha][p].set_cor(color);
+				break;
+			}else{ cout<<"ERRO";
+			}
+		}
+		void imprime(void){
+			int i, j;
+			for(i=0;i<9;i++){
+				for(j=0;j<4;j++){					
+					cout<<"["<<b[i][j].get_cor()<<"]";
+				}
+			cout<<endl;
+			}
+		}
+		void cria(void){
+			int i, j;
+			string c="Buraco";
+			for(i=0;i<9;i++){
+				for(j=0;j<4;j++){					
+					b[i][j].set_cor(c);
+				}
+			}
+		}
+int cor_pertence(string c){
+	for(int i=0;i<4;i++){
+		if(c==cores[i]) return 1;
+	}
+	return 0;
+}
+void set_cores(string * v){cores=v;}
+};
 
 int main(void){
-    int q_cores, t_cod, q_palpites, type;
-    string rep;
-    bool c_rep;
-   //-------------------------------------------Coleta de dados do tipo de jogo-----------------------------------------------------------
-    cout<<"Insira a quantidade de cores desejada(entre 4 e 10):";
-    cin>>q_cores;
-    if(q_cores<4 || q_cores>10){
-        cout<<"Valor inválido"<<endl;
-        return 0;
-    }
-
-    cout<<"Escolha o tamanho do código(entre 4 e 6):";
-    cin>>t_cod;
-    if(t_cod<4 || t_cod>6){
-        cout<<"Valor inválido";
-        return 0;
-    }
-
-    cout<<"Escolha a quantidade de palpites(entre 4 e 10):";
-    cin>>q_palpites;
-    if(q_palpites<4 || q_palpites>10){
-       cout<<"Valor inválido"<<endl;
-       return 0;
-    }
-
-    cout<<"Se você deseja jogar 1x1 digite um, caso queira jogar contra a máquina digite qualquer número:";
-    cin>>type;
-
-    cout<<"Deseja cores repetidas, Sim ou Não:";
-    cin>>rep;
-    if(rep=="Sim")c_rep=true;
-    else c_rep=false;
-    //-------------------------------------------------------------------------------------------------------------------------------------
-
-    config_jogo game(q_cores,t_cod,q_palpites,type, c_rep);
-    if(game.get_tipo()==1){
-        tabuleiro_PvP tijolo(...);        
-        int i=0;
-        int j=0;
-        string color="bacate";
-        for(i=0;i<game.get_palpites();i++){
-            while(color!="pronto"){
-                cout<<"Selecione a Posição e a cor desejada respectivamente: "<<endl;
-                    for(j=0;j<game.get_Tcod;j++){
-                        tijolo.set_posicao(p,color,i);
-                
-    return 0;
-}
+	jogo_pc jogo;
+	jogo.cria();
+	jogo.imprime();
+	jo.set_cores(
+	string c;
+	string v[4]={"Vermelho", "Verde", "Azul", "Preto"};
+int i=0;
+int p;
+	while(i<9){	
+		while(1){
+			cin<<p<<c;	
+			jogo.set_pos(i,p,c);
+			if(p==-1) break;
+		}
+		i++;
+	}
+	return 0;
+}	
